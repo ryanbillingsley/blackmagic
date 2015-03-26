abce58305d4610f9574ee2faa25e12ddbb436d51
ryanbillingsley <ryanbbillingsley@gmail.com>
2015-03-26 11:22:32 -0400

    Major work on getting the cli hooked up to the data

--- cmds/bmcli/main.go
+++ cmds/bmcli/main.go
@@ -1,74 +1,89 @@
 package main
 
 import (
+	"flag"
 	"log"
-	"os"
+	//"os"
 	"time"
 
 	"github.com/nsf/termbox-go"
-	"github.com/ryanbillingsley/termui"
+	"github.com/ryanbillingsley/blackmagic"
+	"gopkg.in/mgo.v2/bson"
 )
+
+import ui "github.com/ryanbillingsley/termui"
 
 type paragraph struct {
 	text   string
-	color  termui.Attribute
+	color  ui.Attribute
 	offset int
 }
 
 func main() {
-	f, err := os.OpenFile("testlogfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	mongoUrl := flag.String("mongo", "localhost", "The mongo db address.  It can be as simple as `localhost` or involved as `mongodb://myuser:mypass@localhost:40001,otherhost:40001/mydb`")
+	databaseName := flag.String("db", "blackmagic", "The name of the database you are connecting to.  Defaults to blackmagic")
+	flag.Parse()
+
+	//f, err := os.OpenFile("testlogfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	//if err != nil {
+	//panic(err)
+	//}
+	//defer f.Close()
+
+	//log.SetOutput(f)
+
+	db := blackmagic.NewDatabase(*mongoUrl, *databaseName)
+	err := db.Connect()
 	if err != nil {
 		panic(err)
 	}
-	defer f.Close()
 
-	log.SetOutput(f)
-
-	err = termui.Init()
+	err = ui.Init()
 	if err != nil {
 		panic(err)
 	}
-	defer termui.Close()
+	defer ui.Close()
 
-	termui.UseTheme("helloworld")
+	ui.UseTheme("helloworld")
 
-	lc := termui.NewLineChart()
-	lc.Border.Label = "dot-mode Line Chart"
+	lc := ui.NewLineChart()
+	lc.Border.Label = "24 Hour Temperature"
 	lc.Mode = "dot"
-	lc.Data = []float64{65.4, 65.8, 66, 66.2, 66.25, 68.75, 65.3, 62.4}
+	lc.Data = []float64{}
 	lc.Width = 77
 	lc.Height = 16
 	lc.X = 0
 	lc.Y = 0
-	lc.AxesColor = termui.ColorWhite
-	lc.LineColor = termui.ColorCyan | termui.AttrBold
+	lc.AxesColor = ui.ColorWhite
+	lc.LineColor = ui.ColorCyan | ui.AttrBold
 
-	parC := termui.NewPar("")
-	parC.Height = 9
-	parC.Width = 37
-	parC.Y = 0
-	parC.X = 77
-	parC.Border.Label = "Temperatures"
-	parC.Border.FgColor = termui.ColorYellow
+	//parC := termui.NewPar("")
+	//parC.Height = 9
+	//parC.Width = 37
+	//parC.Y = 0
+	//parC.X = 77
+	//parC.Border.Label = "Temperatures"
+	//parC.Border.FgColor = termui.ColorYellow
 
-	temps := []paragraph{
-		{text: "High: 65.54", color: termui.ColorWhite, offset: 1},
-		{text: "Predicted High: 68 <---> 66", color: termui.ColorRed, offset: 2},
-		{text: "Low: 65.54", color: termui.ColorWhite, offset: 4},
-		{text: "Predicted Low: 68 <---> 66", color: termui.ColorBlue, offset: 5},
-	}
+	//temps := []paragraph{
+	//{text: "High: 65.54", color: termui.ColorWhite, offset: 1},
+	//{text: "Predicted High: 68 <---> 66", color: termui.ColorRed, offset: 2},
+	//{text: "Low: 65.54", color: termui.ColorWhite, offset: 4},
+	//{text: "Predicted Low: 68 <---> 66", color: termui.ColorBlue, offset: 5},
+	//}
 
-	widgets := []termui.Bufferer{}
+	//widgets := []termui.Bufferer{}
 
-	widgets = append(widgets, lc)
-	widgets = append(widgets, parC)
+	//widgets = append(widgets, lc)
+	//widgets = append(widgets, parC)
 
-	for _, t := range temps {
-		widgets = append(widgets, createPar(t.text, t.offset, t.color))
-	}
+	//for _, t := range temps {
+	//widgets = append(widgets, createPar(t.text, t.offset, t.color))
+	//}
 
-	draw := func() {
-		termui.Render(widgets...)
+	draw := func(data []float64) {
+		lc.Data = data
+		ui.Render(lc)
 	}
 
 	evt := make(chan termbox.Event)
@@ -78,21 +93,52 @@ func main() {
 		}
 	}()
 
+	poll := make(chan []blackmagic.Reading)
+	go func() {
+		for {
+			cd, err := db.Collection("days")
+			cr, err := db.Collection("readings")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			var d blackmagic.Day
+			t := time.Now().Local()
+			year, month, day := t.Date()
+			err = cd.Find(bson.M{"year": year, "month": month, "day": day}).One(&d)
+
+			var readings []blackmagic.Reading
+			err = cr.Find(bson.M{"day": d.Id}).Sort("createdat").All(&readings)
+			if err != nil {
+				log.Fatal(err)
+			} else {
+				poll <- readings
+			}
+
+			time.Sleep(time.Minute * 5)
+		}
+	}()
+
 	for {
 		select {
 		case e := <-evt:
 			if e.Type == termbox.EventKey && e.Ch == 'q' {
 				return
 			}
-		default:
-			draw()
-			time.Sleep(time.Second / 2)
+		case r := <-poll:
+			data := make([]float64, 0)
+			for _, rd := range r {
+				data = append(data, rd.Temperature)
+			}
+
+			draw(data)
+			//log.Println("Received reading", data)
 		}
 	}
 }
 
-func createPar(text string, offset int, color termui.Attribute) *termui.Par {
-	par := termui.NewPar(text)
+func createPar(text string, offset int, color ui.Attribute) *ui.Par {
+	par := ui.NewPar(text)
 	par.Height = 1
 	par.Width = 30
 	par.X = 79
